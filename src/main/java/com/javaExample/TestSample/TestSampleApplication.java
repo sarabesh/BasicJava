9263b371ffd047c7674cf4c827c09e43014baed2
sarabesh ravindranath <sarabesh.ravindranath@coda.global>
2019-06-21 15:57:26 +0530

    test commit

---
+++ src/main/java/com/javaExample/TestSample/TestSampleApplication.java
@@ -0,0 +1,13 @@
+package com.javaExample.TestSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaExample/TestSample/TestSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javaExample.TestSample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
